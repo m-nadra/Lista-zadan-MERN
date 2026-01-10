7ea4cb12844c4584c02575da90c02c417776a956
Mikolaj Nadra <m.nadra@proton.me>
2026-01-10 18:10:38 +0100

    client: add tasks display in table

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,3 +1,46 @@
-export default function Main(){
-    return "Main"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router"
+
+export default function Main() {
+    const navigate = useNavigate()
+    const [tasks, setTasks] = useState()
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:3000/api/tasks", {
+                method: "GET",
+                credentials: "include"
+            })
+            if (response.status === 401) {
+                return navigate("/login")
+            }
+            const data = await response.json()
+            setTasks(data)
+        }
+        fetchData()
+            .catch(() => {
+                return navigate("/login")
+            })
+    }, [navigate])
+    
+    return <div>
+        <h1>Lista zadań</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nazwa</th>
+                    <th>Opis</th>
+                    <th>Data wykonania</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks && tasks.map(task => (
+                    <tr key={task.id}>
+                        <td>{task.name}</td>
+                        <td>{task.description}</td>
+                        <td>{task.date}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
 }
