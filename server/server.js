8c48ef4b123e480156367d5805d1fe8c584f3caf
Mikolaj Nadra <m.nadra@proton.me>
2026-01-13 12:21:50 +0100

    server: add morgan

--- server/server.js
+++ server/server.js
@@ -5,20 +5,23 @@ import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 import tasks from './routes/tasks.js'
 import auth from './routes/auth.js'
+import morgan from 'morgan';
 
 const port = process.env.PORT
 const app = express()
 
-mongoose.connect(process.env.DATABASE_URL)
-    .then(() => console.log("Connected to the database"))
-    .catch(err => console.error(err))
-
+app.use(morgan(':date - :method :url - :status'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
 }))
+
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => console.log("Connected to the database"))
+    .catch(err => console.error(err))
+
 app.use('/api', tasks)
 app.use('/', auth)
 
